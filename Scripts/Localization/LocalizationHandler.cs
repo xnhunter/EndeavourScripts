55f35e6f0d0937f064bd685b756624cc39bbe1e1
Valentyn Bondarenko <bond.id@outlook.com>
2021-07-16 14:07:22 +0300

    Add files via upload

---
+++ Scripts/Localization/LocalizationHandler.cs
@@ -0,0 +1,108 @@
+﻿using System.IO;
+using System.Threading;
+using System.Collections;
+using System.Globalization;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using Newtonsoft.Json.Linq;
+
+public class LocalizationHandler : MonoBehaviour {
+	public Text LanguageMenuLabel;
+	public Toggle FollowSystemLanguage;
+
+	private  Dictionary<string, string> Locales;
+
+	static string LocalizationPath = @"Localization/";
+	private string SystemLanguage;
+	private TextAsset LocaleAsset;
+
+	static public string JSONFileText;
+	public static JObject parsedJSONText;
+
+	public static string CurrentLanguage;
+
+	// Use this for initialization
+	void Awake () {
+		
+		Locales = new Dictionary<string, string>();
+		Locales.Add ("en", "English");
+		Locales.Add ("ua", "Ukrainian");
+		Locales.Add ("ru", "Russian");
+
+		JSONFileText = string.Empty;
+		parsedJSONText = new JObject();
+		
+		LocaleAsset = new TextAsset ();
+		
+		LanguageMenuLabel.text = PlayerPrefs.GetString ("Language", "English");
+		CurrentLanguage = LanguageMenuLabel.text;
+
+		var temp = PlayerPrefs.GetInt ("FolloSystemLanguage", 1);
+		if (temp == 1)
+			FollowSystemLanguage.isOn = true;
+		else
+			FollowSystemLanguage.isOn = false;
+
+		SystemLanguage = Application.systemLanguage.ToString ();
+
+		UpdateLanguage (FollowSystemLanguage.isOn);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!(FollowSystemLanguage.isOn) && (LanguageMenuLabel.text != PlayerPrefs.GetString("Language")))
+		{
+			UpdateLanguage(false);
+
+		}
+		else if (FollowSystemLanguage.isOn && (LanguageMenuLabel.text != SystemLanguage))
+		{
+			UpdateLanguage(true);
+		}
+	}
+
+	public void UpdateLanguage(bool followSystemSettings)
+	{
+		foreach (var loc in Locales) {
+			if (loc.Value == (followSystemSettings ? SystemLanguage : LanguageMenuLabel.text )) {
+				LocaleAsset = Resources.Load (LocalizationPath + loc.Key) as TextAsset;
+				LanguageMenuLabel.text = loc.Value;
+				PlayerPrefs.SetString ("Language", loc.Value);
+
+				CurrentLanguage = loc.Value;
+
+				UpdateLocale (loc.Key);
+
+				break;
+			}
+		}
+
+		JSONFileText = LocaleAsset.text;
+
+		if (followSystemSettings) {
+
+			// Couldn't math system language with existed ones.
+			if(JSONFileText == string.Empty)
+			{
+				Awake();
+				return;
+			}
+
+			PlayerPrefs.SetInt ("FolloSystemLanguage", 1);
+		}
+		else
+			PlayerPrefs.SetInt ("FolloSystemLanguage", 0);
+
+
+		parsedJSONText = JObject.Parse (LocalizationHandler.JSONFileText);
+	}
+
+	public void UpdateLocale(string locale)
+	{
+		Debug.Log ("Locale Updated: " + locale);
+	}
+}
---
+++ Scripts/Localization/LocalizationTextBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class LocalizationTextBehaviour : MonoBehaviour {
+	private Text text;
+
+	private Font CyrilicFont;
+	private Font AquireFont;
+
+	private string lastLanguage;
+	void Start ()
+	{
+		text = this.GetComponent<Text> ();
+		
+		CyrilicFont = Resources.Load<Font>(@"kz-taurus");
+		AquireFont = text.font;
+
+		UpdateLocalization();
+	}
+
+	public void Update()
+	{
+		// Update a text ony if the language was changed.
+		if(lastLanguage != LocalizationHandler.CurrentLanguage)
+			UpdateLocalization();
+	}
+
+	public void UpdateLocalization()
+    {
+		if (text != null)
+		{
+			// Switch fonts to cyrilic and backwards.
+			if (LocalizationHandler.CurrentLanguage == "Russian" || LocalizationHandler.CurrentLanguage == "Ukrainian")
+			{
+				if (text.font.fontNames[0] == "Aquire")
+					text.font = CyrilicFont;
+			}
+			else if (text.font.fontNames[0] == "KZ Taurus")
+				text.font = AquireFont;
+
+
+			// Localize text by finding the right pair.
+			foreach (var pair in LocalizationHandler.parsedJSONText)
+			{
+				var parent = transform.parent.name;
+
+				if (parent.Contains(pair.Key))
+				{
+					text.text = pair.Value.ToString();
+				}
+			}
+		
+			lastLanguage = LocalizationHandler.CurrentLanguage;
+		}
+	}
+}
