72a5a265483d04a557a6fdb7c7a8de3c8dc05000
Valentyn Bondarenko <bond.id@outlook.com>
2021-07-21 11:45:24 +0300

    Update Bullet.cs

--- Scripts/Bullet.cs
+++ Scripts/Bullet.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,8 +19,5 @@ public abstract class Bullet : MonoBehaviour {
 	void Update () {
 		
 	}
-	virtual public void Hit()
-	{
-
-	}
+	abstract public void Hit();
 }
